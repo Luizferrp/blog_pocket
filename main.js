004463f5552ceb509681c0ea8648d83db22934c2
Luiz Fernando Reis Pereira <103222344+Luizferrp@users.noreply.github.com>
2026-09-12 20:53:27 -0300

    adding ruffman compressor prototype to the articles

---
+++ main.js
@@ -0,0 +1,15 @@
+import { HuffmanCompressor } from './src/compressor.js';
+
+const compressor = new HuffmanCompressor();
+
+// 1. Baixar e carregar a htree.dat global
+const htreeRes = await fetch('/cache/htree.dat');
+const htreeArrayBuffer = await htreeRes.arrayBuffer();
+compressor.deserializeTree(new Uint8Array(htreeArrayBuffer));
+
+// 2. Baixar e descomprimir um artigo sob demanda
+const articleRes = await fetch('/articles/artigo_001.dat');
+const articleArrayBuffer = await articleRes.arrayBuffer();
+const textoOriginal = compressor.decode(new Uint8Array(articleArrayBuffer));
+
+console.log("Artigo Renderizado:", textoOriginal);
