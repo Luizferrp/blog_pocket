a82827326e0f855dd09def6597d11cb024f024e3
Luiz Fernando Reis Pereira <103222344+Luizferrp@users.noreply.github.com>
2026-09-12 23:23:35 -0300

    fixing url

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ import { HuffmanCompressor } from './src/compressor.js';
 const compressor = new HuffmanCompressor();
 
 // 1. Baixar e carregar a htree.dat global
-const htreeRes = await fetch('/cache/htree.dat');
+const htreeRes = await fetch('/blog_pocket/cache/htree.dat');
 const htreeArrayBuffer = await htreeRes.arrayBuffer();
 compressor.deserializeTree(new Uint8Array(htreeArrayBuffer));
 
 // 2. Baixar e descomprimir um artigo sob demanda
-const articleRes = await fetch('/articles/artigo_001.dat');
+const articleRes = await fetch('/blog_pocket/articles/artigo_001.dat');
 const articleArrayBuffer = await articleRes.arrayBuffer();
 const textoOriginal = compressor.decode(new Uint8Array(articleArrayBuffer));
 
