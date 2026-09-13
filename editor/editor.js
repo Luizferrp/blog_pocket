037f91a9f9d04d809ea86329e8f8eb308c5ce84b
luizp <luizp@luizps-MacBook-Pro.local>
2026-09-13 02:05:09 -0300

    fixing file fetch

--- editor/editor.js
+++ editor/editor.js
@@ -61,7 +61,7 @@ async function init() {
   
   // 1.2 Tenta carregar os dados (Tolerante a repositório vazio)
   try {
-    const treeRes = await fetch('/cache/tree.dat').catch(() => null);
+    const treeRes = await fetch('./../cache/tree.dat').catch(() => null);
     if (treeRes && treeRes.ok) {
       const buffer = await treeRes.arrayBuffer();
       if (buffer.byteLength > 0 && typeof state.treeManager.deserialize === 'function') {
@@ -80,7 +80,7 @@ async function init() {
       }
     }
 
-    const htreeRes = await fetch('/cache/htree.dat').catch(() => null);
+    const htreeRes = await fetch('./../cache/htree.dat').catch(() => null);
     if (htreeRes && htreeRes.ok) state.htreeData = await htreeRes.arrayBuffer();
 
   } catch (err) {
