2975208ef85e9f8aad6b7264f02a73129486abb1
luizp <luizp@luizps-MacBook-Pro.local>
2026-09-19 18:20:44 -0300

    skill isseue

--- editor/editor.js
+++ editor/editor.js
@@ -182,7 +182,7 @@ async function loadArticle(articleId) {
   showStatus("Lendo arquivo...");
 
   try {
-    file = `/blog_pocket/articles/${articleId}.md`;
+    const file = `/blog_pocket/articles/${articleId}.md`;
     console.log(file);
     const res = await fetch(file).catch(() => null);
     if (res && res.ok) {
